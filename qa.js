f7f0c78a62c6f1080db658875fccc73912501177
Tsukiyomi Yana <tsukiyomiyana@gmail.com>
2026-02-23 20:35:27 -0500

    Update qa.js

--- qa.js
+++ qa.js
@@ -91,8 +91,8 @@
           <!-- RIGHT: photos -->
           <div class="profile-media">
             <div class="media-grid">
-              <img src="https://cdn.jsdelivr.net/gh/TsukiyomiYana/yana-portfolio-assets@master/thumbs/web-yanaid-s01-962x1200-v1.webp" alt="Character Photo 1">
-              <img src="https://YOUR_IMAGE_URL_2.png" alt="Character Photo 2">
+              <img src="https://cdn.jsdelivr.net/gh/TsukiyomiYana/yana-portfolio-assets@master/thumbs/web-yanaid-s01-962x1200-v1.webp" alt="Tsukiyomi Yana Photo ID">
+              <img src="https://YOUR_IMAGE_URL_2.png" alt="Anu Photo ID">
             </div>
 
             <!-- 單張版本（要單張就把上面 media-grid 刪掉，改用這行） -->
